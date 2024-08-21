4acb61decf0f642a87324c261f6ba374daeb2a62
Michal Kanarek <michal.kanarek@gmail.com>
2024-08-21 20:33:34 +0200

    Fix irrigation

--- backend/src/helpers/startIrrigation.ts
+++ backend/src/helpers/startIrrigation.ts
@@ -1,13 +1,10 @@
-if (process.env.DRY_RUN === 'true') {
-  var Gpio = require('onoff').Gpio
-  var PIN = new Gpio(516, 'out', { initial: 1 }) // on rasberyPI it is physical 7
-} else {
-  //@ts-ignore
-  var PIN = { writeSync: () => {} }
-}
+var Gpio = require('onoff').Gpio;
+var PIN = new Gpio(516, 'out', { initial: 1 }); // on rasberyPI it is physical 7
+
 
 export const startIrrigation = async () => {
   PIN.writeSync(0)
 }
+
 
 startIrrigation().then()
--- backend/src/helpers/stopIrrigation.ts
+++ backend/src/helpers/stopIrrigation.ts
@@ -1,13 +1,10 @@
-if (process.env.DRY_RUN === 'true') {
-  var Gpio = require('onoff').Gpio
-  var PIN = new Gpio(516, 'out', { initial: 1 }) // on rasberyPI it is physical 7
-} else {
-  //@ts-ignore
-  var PIN = { writeSync: () => {} }
-}
+var Gpio = require('onoff').Gpio;
+var PIN = new Gpio(516, 'out', { initial: 1 }); // on rasberyPI it is physical 7
+
 
 export const stopIrrigation = async () => {
-  PIN.writeSync(1)
+  PIN.writeSync(0)
 }
+
 
 stopIrrigation().then()
